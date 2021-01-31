992bbf157505eacc5711c2d14b30c356c3ecbc73
Andrew Manzanero <manzanero.andrew@gmail.com>
2021-01-31 13:45:19 -0800

    add code linting

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+    sourceType: "module",
+  },
+  rules: {
+    "react/prop-types": 0,
+    "react/react-in-jsx-scope": 0,
+  },
+  ignorePatterns: ["/node_modules/**", "/build/**"],
+};
