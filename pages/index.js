3bd772346c07f8dc041df6098094f86dba2b0742
Michael Chang <chan302@usc.edu>
2021-02-08 07:43:08 -0800

    Chan/landing (#6)

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,8 @@
 import Head from "next/head";
 import Navbar from "../components/navbar";
+import Button from "../components/button";
+
+// import Landing from "./Landing/Landing";
 
 export default function Home() {
   return (
@@ -8,6 +11,24 @@ export default function Home() {
         <title>Heirloom</title>
       </Head>
       <Navbar />
+      <div className="landing">
+        <div className="landing__left">
+          <div className="landing__header">
+            Keep memories of <br />
+            loved ones alive.
+          </div>
+          <div className="landing__subheader">
+            Connect and share stories with your community.
+          </div>
+          <div className="landing__header__button">
+            <Button variant="filled"> Learn more</Button>
+          </div>
+        </div>
+
+        <div className="landing__right">
+          <img className="gramps" />
+        </div>
+      </div>
     </>
   );
 }
