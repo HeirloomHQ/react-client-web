27b3c4aebb80656a8d9f435053fd89a6ba114eda
Andrew Manzanero <manzanero.andrew@gmail.com>
2021-02-08 21:50:37 -0800

    demo code

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,29 @@
+import { useState } from "react";
 import Head from "next/head";
+import axios from "axios";
 import Navbar from "../components/navbar";
 import Button from "../components/button";
 
 export default function Home() {
+  const [message, setMessage] = useState("initial state");
+  async function onButtonClick() {
+    try {
+      const response = await axios.get("/api");
+      const { data } = response;
+      setMessage(data.message);
+    } catch (e) {
+      window.alert(e);
+    }
+  }
+
   return (
     <>
       <Head>
         <title>Heirloom</title>
       </Head>
       <Navbar />
+      <button onClick={onButtonClick}>mybutton</button>
+      <p>{message}</p>
       <div className="landing">
         <div className="landing__left">
           <div className="landing__header">
