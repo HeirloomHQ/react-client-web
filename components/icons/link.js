965d53c9e0607040437d4ccfb43d67ee25d2bda5
Andrew Manzanero <manzanero.andrew@gmail.com>
2021-02-24 21:22:46 -0800

    add more icons

---
+++ components/icons/link.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+export default function Link({ className }) {
+  return (
+    <svg
+      className={className}
+      width="21"
+      height="20"
+      viewBox="0 0 21 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M14.772 10.5527L17.9578 7.43802C19.3583 6.06872 19.3583 3.84864 17.9578 2.47934V2.47934C16.5572 1.11004 14.2865 1.11004 12.886 2.47934L9.70023 5.59404"
+        stroke="#1F1F1F"
+        strokeWidth="1.3"
+        strokeLinecap="round"
+      />
+      <path
+        d="M6.22163 9.00002L3.03588 12.1147C1.63534 13.484 1.63535 15.7041 3.03588 17.0734V17.0734C4.43642 18.4427 6.70714 18.4427 8.10767 17.0734L11.2934 13.9587"
+        stroke="#1F1F1F"
+        strokeWidth="1.3"
+        strokeLinecap="round"
+      />
+      <path
+        d="M8.37183 12L12.3718 8"
+        stroke="black"
+        strokeWidth="1.3"
+        strokeLinecap="round"
+      />
+    </svg>
+  );
+}
