4c1e4a7cc95717caae4f6dec77a3eb58afec0b4f
Joses <jgaldamez102@gmail.com>
2021-03-14 19:59:58 -0700

    Adding all files

--- pages/page.js
+++ pages/page.js
@@ -4,33 +4,31 @@ import Navbar from "../components/navbar";
 import Button from "../components/button";
 import PageNavbar from "../components/pageNavbar";
 
+import React from "react";
+import BubbleElement, { defaultOptions } from "../lib/react-bubble-ui";
+import companyData from "../components/companies";
+import CompanyBubble from "../components/test";
+
 export default function Home() {
 	const router = useRouter();
 	console.log(router.query.mem_id);
 	const memorial_id = router.query.mem_id
+	const firstname = router.query?.firstname
+	const lastname = router.query?.lastname
   return (
     <>
       <Head>
-        <title>Heirloom</title>
+        <title>{firstname}</title>
       </Head>
 	  <PageNavbar />
       <div>
         <div className="landing bg-paper w-full h-full">
-          <div className="landing__left">
-            <div className="landing__header">
-              Keep memories of <br />
-              loved ones alive.
-            </div>
-            <div className="landing__subheader">
-              Connect and share stories with your community. 
-            </div>
-            <div className="landing__header__button">
-              <Button variant="filled"> Learn more</Button>
-            </div>
-          </div>
-
-          <div className="landing__right">
-            <img alt="couple holding hands" className="gramps" />
+		  <div className="bubble-container">
+				<BubbleElement options={defaultOptions} className="bubbleUI">
+				{companyData.slice(0, 20).map((company, i) => (
+					<CompanyBubble {...company} key={i} />
+				))}
+				</BubbleElement>
           </div>
         </div>
       </div>
