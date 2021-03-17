62f53ebe8ad8c99b65aca7382cc3a5b35deb4a12
Andrew Manzanero <manzanero.andrew@gmail.com>
2021-03-17 16:24:18 -0700

    update dash nav

--- components/dashNavbar.js
+++ components/dashNavbar.js
@@ -1,33 +1,44 @@
 import React from "react";
 import Image from "next/image";
 import { useRouter } from "next/router";
+import IconButton from "@material-ui/core/IconButton";
+import AddIcon from "@material-ui/icons/Add";
+import NotifIcon from "@material-ui/icons/Notifications";
 import NavLink from "./navLink";
+import { Tooltip } from "@chakra-ui/react";
+import { useAuth } from "../lib/clientSideAuth";
 
 export default function DashNavbar() {
   const router = useRouter();
+  const userCtx = useAuth();
   return (
     <>
       <div className="w-full flex items-center px-24 py-3 bg-paper">
         <Image
-          src="/assets/img/heirloom-logo-no-txt.png"
+          src="/assets/img/nav-logo.png"
           alt="Heirloom logo"
-          height={30}
-          width={30}
+          height={36}
+          width={147}
           onClick={() => router.push("/")}
         />
         <div className="ml-auto flex">
-          <NavLink
-            className="font-body flex items-center"
-            onClick={() => window.alert("help")}
-          >
-            Help
-          </NavLink>
-          {/*temp dropdown*/}
-          <div className="flex items-center ml-4">
-            <Image src="/assets/img/account-circle.svg" width={36} height={36} />
-            <div className="font-body ml-4 mr-2">Account</div>
-            <Image src="/assets/img/down.svg" width={10} height={10} />
-          </div>
+          <Tooltip hasArrow label="Create New Heirloom" bg="blue.800" color="white">
+            <IconButton style={{ "margin-right": "1rem" }} size="small">
+              <AddIcon className="text-black" fontSize="large" />
+            </IconButton>
+          </Tooltip>
+          <Tooltip hasArrow label="Notifications" bg="blue.800" color="white">
+            <IconButton style={{ "margin-right": "1rem" }} size="small">
+              <NotifIcon className="text-black" fontSize="large" />
+            </IconButton>
+          </Tooltip>
+          <Tooltip hasArrow label="Account Settings" bg="blue.800" color="white">
+            <button className="bg-heirloomOrange rounded-full w-9 h-9 my-auto">
+              <span className="flex items-center justify-center text-white  box-border">
+                {userCtx.user?.firstName.length ? userCtx.user?.firstName[0] : ""}
+              </span>
+            </button>
+          </Tooltip>
         </div>
       </div>
       <hr />
