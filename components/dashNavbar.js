4efbe8420760efccd34aa24e5ff5b5b9a575daf4
Andrew Manzanero <manzanero.andrew@gmail.com>
2021-03-23 21:55:51 -0700

    add create context to dasnav

--- components/dashNavbar.js
+++ components/dashNavbar.js
@@ -4,13 +4,17 @@ import { useRouter } from "next/router";
 import IconButton from "@material-ui/core/IconButton";
 import AddIcon from "@material-ui/icons/Add";
 import NotifIcon from "@material-ui/icons/Notifications";
-import NavLink from "./navLink";
 import { Tooltip } from "@chakra-ui/react";
 import { useAuth } from "../lib/clientSideAuth";
+import { useHeirloomCreatContext } from "./createHeirloom/hooks";
 
 export default function DashNavbar() {
   const router = useRouter();
   const userCtx = useAuth();
+
+  const [_, dispatch] = useHeirloomCreatContext();
+  const open = () => dispatch({ type: "OPEN" });
+
   return (
     <>
       <div className="w-full flex items-center px-24 py-3 bg-paper">
@@ -23,7 +27,7 @@ export default function DashNavbar() {
         />
         <div className="ml-auto flex">
           <Tooltip hasArrow label="Create New Heirloom" bg="blue.800" color="white">
-            <IconButton style={{ "margin-right": "1rem" }} size="small">
+            <IconButton style={{ marginRight: "1rem" }} size="small" onClick={open}>
               <AddIcon className="text-black" fontSize="large" />
             </IconButton>
           </Tooltip>
@@ -33,7 +37,7 @@ export default function DashNavbar() {
             </IconButton>
           </Tooltip>
           <Tooltip hasArrow label="Account Settings" bg="blue.800" color="white">
-            <button className="bg-heirloomOrange rounded-full w-9 h-9 my-auto">
+            <button className="bg-heirloomOrange rounded-full w-9 h-9 my-auto hover:bg-heirloomOrange-dark transition-colors">
               <span className="flex items-center justify-center text-white  box-border">
                 {userCtx.user?.firstName.length ? userCtx.user?.firstName[0] : ""}
               </span>
